65d179971c0f073b5ac0b16928d4bd55cbda096d
Sfl <942368681@qq.com>
2021-01-07 15:06:50 +0800

    update

--- mainApp/mock/api.ts
+++ mainApp/mock/api.ts
@@ -6,11 +6,13 @@ export default {
                 name: 'subApp1',
                 // entry: '//192.168.200.18:8001'
                 entry: '//localhost:8001'
+                // entry: 'https://sfl1.test.eastedu.ltd/'
             },
             {
                 name: 'subApp2',
                 // entry: '//192.168.200.18:8002'
                 entry: '//localhost:8002'
+                // entry: 'https://sfl2.test.eastedu.ltd/'
             }
         ]
     },
--- mainApp/src/app.tsx
+++ mainApp/src/app.tsx
@@ -69,6 +69,64 @@ export const qiankun = request('/api/appList', {}).then((res: any) => {
     message.error('子应用初始化失败');
 });
 
+// export const qiankun = new Promise(resolve => {
+//     resolve([
+//         {
+//             name: 'subApp1',
+//             entry: 'https://sfl1.test.eastedu.ltd/',
+//         },
+//         {
+//             name: 'subApp2',
+//             entry: 'https://sfl2.test.eastedu.ltd/',
+//         }
+//     ])
+// }).then(res => {
+//     SUB_APPS = res;
+//     return {
+//         apps: SUB_APPS.map((item: any) => ({
+//             ...item,
+//             props: {
+                
+//             }
+//         })),
+//         sandbox: true,
+//         // routes: SUB_APPS.map((item: any) => ({
+//         //     path: `/subApp/${item.name}`,
+//         //     microApp: item.name,
+//         //     microAppProps: {
+//         //         autoSetLoading: true, // 开启微应用的 loading 动画
+//         //         className: 'micro-container', // 微应用容器 class
+//         //         wrapperClassName: 'micro-wrapper'  // wrapper class，仅开启 loading 动画时生效
+//         //     }
+//         // })),
+//         lifeCycles: {
+//             beforeLoad: (props: any) => {
+//                 // console.log(1111111, props);
+//             },
+//             beforeMount: (props: any) => {
+//                 // console.log(2222222, props);
+//                 getDvaApp()._store.dispatch({
+//                     type: 'mainAppGlobal/setCurrentMountedSubApp',
+//                     currentMountedSubApp: props.name
+//                 });
+//             },
+//             afterMount: (props: any) => {
+//                 // console.log(3333333, props);
+//             },
+//             beforeUnmount: (props: any) => {
+//                 // console.log(4444444, props);
+//                 getDvaApp()._store.dispatch({
+//                     type: 'mainAppGlobal/setCurrentMountedSubApp',
+//                     currentMountedSubApp: ''
+//                 });
+//             },
+//             afterUnmount: (props: any) => {
+//                 // console.log(5555555, props);
+//             }
+//         }
+//     };
+// });
+
 export function rootContainer(container: any) {
     return (
         <AppProvider subApps={SUB_APPS}>
